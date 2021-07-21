c23c941a994f1d50faf587fe7b036c2283b22f8e
Dylan Marcus <dylan@mach1.tech>
2021-07-21 12:39:43 -0400

    more bugfixes for UE5 support

--- Mach1DecodePlugin/SourcePlugin/Mach1DecodePlugin/Source/Mach1DecodePlugin/Mach1DecodePlugin.Build.cs
+++ Mach1DecodePlugin/SourcePlugin/Mach1DecodePlugin/Source/Mach1DecodePlugin/Mach1DecodePlugin.Build.cs
@@ -85,6 +85,7 @@ namespace UnrealBuildTool.Rules
                     PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, arch, "libMach1EncodeCAPI.a"));
                     PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, arch, "libMach1DecodeCAPI.a"));
                     PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, arch, "libMach1DecodePositionalCAPI.a"));
+                    PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, arch, "libMach1TranscodeCAPI.a"));
                 }
             }
             else if (Target.Platform == UnrealTargetPlatform.IOS)
@@ -92,26 +93,31 @@ namespace UnrealBuildTool.Rules
                 PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "libMach1EncodeCAPI.a"));
                 PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "libMach1DecodeCAPI.a"));
                 PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "libMach1DecodePositionalCAPI.a"));
+                PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "libMach1TranscodeCAPI.a"));
             }
             else if (Target.Platform == UnrealTargetPlatform.Mac)
             {
                 PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "libMach1EncodeCAPI.a"));
                 PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "libMach1DecodeCAPI.a"));
                 PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "libMach1DecodePositionalCAPI.a"));
+                PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "libMach1TranscodeCAPI.a"));
             }
             else if (Target.Platform == UnrealTargetPlatform.Win64)
             { 
                 PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "Mach1EncodeCAPI.lib"));
                 PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "Mach1DecodeCAPI.lib"));
                 PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "Mach1DecodePositionalCAPI.lib"));
-                 
+                PublicAdditionalLibraries.Add(Path.Combine(Mach1BinDirectory, "Mach1TranscodeCAPI.lib"));
+
                 RuntimeDependencies.Add(Path.Combine(Mach1BinDirectory, "Mach1EncodeCAPI.dll"));
                 RuntimeDependencies.Add(Path.Combine(Mach1BinDirectory, "Mach1DecodeCAPI.dll"));
                 RuntimeDependencies.Add(Path.Combine(Mach1BinDirectory, "Mach1DecodePositionalCAPI.dll"));
+                RuntimeDependencies.Add(Path.Combine(Mach1BinDirectory, "Mach1TranscodeCAPI.dll"));
 
                 CopyToBinaries(Path.Combine(Mach1BinDirectory, "Mach1EncodeCAPI.dll"), Target);
                 CopyToBinaries(Path.Combine(Mach1BinDirectory, "Mach1DecodeCAPI.dll"), Target);
                 CopyToBinaries(Path.Combine(Mach1BinDirectory, "Mach1DecodePositionalCAPI.dll"), Target);
+                CopyToBinaries(Path.Combine(Mach1BinDirectory, "Mach1TranscodeCAPI.dll"), Target);
             }
 
         }
